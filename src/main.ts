bc1ea2ee4275c5e09083e7b825fa426945496a22
Prathamesh Mutkure <pmutkure009@gmail.com>
2024-07-09 17:06:18 +0530

    Swagger Docs

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,31 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle('AutoStellar API')
+    .setDescription('Auto Stellar API description')
+    .setVersion('1.0')
+    .addTag('learn-ao')
+    .addBearerAuth(
+      {
+        description: `Please enter token in following format: Bearer <JWT>`,
+        name: 'Authorization',
+        bearerFormat: 'Bearer',
+        scheme: 'Bearer',
+        type: 'http',
+        in: 'Header',
+      },
+      'access-token',
+    )
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
 
   await app.listen(8000);
 }
